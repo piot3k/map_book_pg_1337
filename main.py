9a033ee7bc1eb5168db0de3062b4099dae0dc438
maciejjerczynski998 <maciejjerczynski998@gmail.com>
2025-04-15 18:30:28 +0200

    wip: add new user functionality

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from utils.model import users
-from utils.controller import get_user_info
+from utils.controller import get_user_info, add_user
 
 
 def main():
     print('============MENU=============')
     print('0 - zakończ program')
     print('1 - wyświetl co u znajomych')
+    print('2 - dodaj znajomego')
     print('=============================')
     while True:
         choice: str = input('wybierz opcję MENU: ')
         if choice == '0': break
         if choice == '1': get_user_info(users)
+        if choice == '2': add_user(users)
+
 
 
 if __name__ == '__main__':
