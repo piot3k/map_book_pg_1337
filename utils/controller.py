844c32b14062778379966932f558c8232c33a9d3
maciejjerczynski998 <maciejjerczynski998@gmail.com>
2025-04-15 19:10:56 +0200

    wip: update user functionality

--- utils/controller.py
+++ utils/controller.py
@@ -9,8 +9,18 @@ def add_user(users_data: list) -> None:
     new_posts: str = input('podaj liczbę postów: ')
     users_data.append({'name': new_name, 'location': new_location, 'posts': new_posts}, )
 
+
 def remove_user(users_data: list) -> None:
     user_name: str = input('podaj imię znajomego do usunięcia: ')
     for user in users_data:
         if user['name'] == user_name:
             users_data.remove(user)
+
+
+def update_user(users_data: list) -> None:
+    user_name: str = input('podaj imię użytkownika do aktualizacji: ')
+    for user in users_data:
+        if user['name'] == user_name:
+            user['name'] = input('podaj nowe imię użytkownika: ')
+            user['location'] = input('podaj nową lokalizację użytkownika: ')
+            user['posts'] = input('podaj liczbę postów: ')
