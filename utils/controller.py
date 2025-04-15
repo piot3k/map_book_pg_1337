df9963674fd6d2f91e16f86eade68f0042dd71e1
maciejjerczynski998 <maciejjerczynski998@gmail.com>
2025-04-15 18:55:28 +0200

    wip: remove new user functionality

--- utils/controller.py
+++ utils/controller.py
@@ -8,3 +8,9 @@ def add_user(users_data: list) -> None:
     new_location: str = input('podaj lokalizację: ')
     new_posts: str = input('podaj liczbę postów: ')
     users_data.append({'name': new_name, 'location': new_location, 'posts': new_posts}, )
+
+def remove_user(users_data: list) -> None:
+    user_name: str = input('podaj imię znajomego do usunięcia: ')
+    for user in users_data:
+        if user['name'] == user_name:
+            users_data.remove(user)
